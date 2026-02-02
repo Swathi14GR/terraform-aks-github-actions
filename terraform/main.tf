795bacf05a2fe19a22ae1e874e45ddc2f50a8017
Swathi14GR <goudarswathi@gmail.com>
2026-02-02 21:28:43 +0530

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,7 @@ resource "azurerm_subnet" "aks_subnet" {
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes     = var.subnet_address_prefixes
 
+
 }
 # -----------------------------
 # Azure Container Registry
@@ -69,10 +70,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   network_profile {
-    network_plugin      = "azure"
-    network_policy      = "azure"
-    service_cidr        = "10.1.0.0/16"
-    dns_service_ip      = "10.1.0.10"
+    network_plugin = "azure"
+    network_policy = "azure"
+    service_cidr   = "10.1.0.0/16"
+    dns_service_ip = "10.1.0.10"  
   }
 
   oms_agent {
