6a5b5090bde3133e4c745f0f70cc88011092b5b6
Swathi14GR <goudarswathi@gmail.com>
2026-02-02 17:36:00 +0530

    LAW

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,10 +49,11 @@ resource "azurerm_container_registry" "acr" {
 # AKS Cluster
 # -----------------------------
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                            = var.aks_name
-  location                        = azurerm_resource_group.rg.location
-  resource_group_name             = azurerm_resource_group.rg.name
-  dns_prefix                       = var.dns_prefix
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
   role_based_access_control_enabled = true
   private_cluster_enabled           = true
 
