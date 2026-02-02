a3bb11c5e9e9a8379d3039538da59c5fdddc29ac
Swathi14GR <goudarswathi@gmail.com>
2026-02-02 13:52:23 +0530

    fmt chcek

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 # Resource Group
-resource "azurerm_resource_group" "rg" {
+  resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
 # Virtual Network
-resource "azurerm_virtual_network" "aks_vnet" {
+  resource "azurerm_virtual_network" "aks_vnet" {
   name                = var.vnet_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
