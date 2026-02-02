d4ef9bf72e61f98ea76c7cb0faf8439353e9584c
Swathi14GR <goudarswathi@gmail.com>
2026-02-02 17:01:35 +0530

    a

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = var.dns_prefix
-
   role_based_access_control_enabled = true
   private_cluster_enabled           = true
 
