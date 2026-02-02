470d69cb8c2ec8bd1695dcf1ab2aa97c7f1a69f9
Swathi14GR <goudarswathi@gmail.com>
2026-02-02 23:04:07 +0530

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,15 +21,6 @@ resource "azurerm_subnet" "aks_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes     = var.subnet_address_prefixes
-
-  delegation {
-    name = "aks-delegation"
-
-    service_delegation {
-      name    = "Microsoft.ContainerService/managedClusters"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
 }
 
 # -----------------------------
