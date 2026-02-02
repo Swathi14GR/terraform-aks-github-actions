f2107cd1e72fcd094e9fb51b62e0416682245241
Swathi14GR <goudarswathi@gmail.com>
2026-02-02 16:24:00 +0530

    private cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.dns_prefix
 
   role_based_access_control_enabled = true
-    private_cluster_enabled  = true
+  private_cluster_enabled  = true
 
   default_node_pool {
     name           = "default"
