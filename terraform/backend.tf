78a155fb4cae71335ec5f3c08279d161f9086c75
Swathi14GR <goudarswathi@gmail.com>
2026-02-02 12:20:05 +0530

    stroge name correction

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-state-rg"
-    storage_account_name = "tfstateprod123tfstateprodassigment"
+    storage_account_name = "tfstateprodassigment"
     container_name       = "tfstate"
     key                  = "infra.tfstate"
   }
